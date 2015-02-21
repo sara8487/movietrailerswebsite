e4885716ce8efa77d9a089f0e108b6426f227d90
sara8487 <saraallenproductions@gmail.com>
2015-02-21 06:52:01 -0500

    Update media.py

--- media.py
+++ media.py
@@ -5,11 +5,15 @@ class Movie ():
 ###Each movie will feature a title, poster image, Youtube trailer, and storyline. 
     def __init__(self, movie_title, movie_storyline, poster_image,
               trailer_youtube):
+                 #the structure of each movie entry
         self.title = movie_title
+        #includes the movie title 
         self.storyline = movie_storyline
+        #includes a basic story summary
         self.poster_image_url = poster_image
+        #includes a movie poster from Wikipedia
         self.trailer_youtube_url = trailer_youtube
-        
+        #launches the youtube trailer in the same browser window
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 ###Clicking on a movie poster will open a Youtbe trailer on the same page
